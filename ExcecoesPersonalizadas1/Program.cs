a457689c87b88b17f583f6b0b55b281dd450e4bc
Lucas Máximo <lucasmaximo@outlook.com>
2024-03-06 20:54:27 -0300

    Adicionar arquivos de projeto.

---
+++ ExcecoesPersonalizadas1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ExcecoesPersonalizadas1;
+using ExcecoesPersonalizadas1.Entities.Exceptions;
+try
+{
+    Console.Write("Número do quarto: ");
+    int numeroQuarto = int.Parse(Console.ReadLine());
+    Console.Write("Data de entrada: ");
+    DateTime checkin = DateTime.Parse(Console.ReadLine());
+    Console.Write("Data de saida: ");
+    DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+    Reserva reserva = new Reserva(numeroQuarto, checkin, checkout);
+
+    Console.WriteLine();
+    Console.WriteLine("DADOS DA RESERVA: ");
+    Console.WriteLine(reserva);
+
+    Console.WriteLine();
+
+    Console.WriteLine("Entre os dados para alterar a reserva: ");
+    Console.Write("Nova data de entrada (dd/MM/yyyy): ");
+    checkin = DateTime.Parse(Console.ReadLine());
+    Console.Write("Nova data de saída (dd/MM/yyyy): ");
+    checkout = DateTime.Parse(Console.ReadLine());
+
+    reserva.AtualizarReserva(checkin, checkout);
+
+    Console.WriteLine();
+    Console.WriteLine("DADOS DA RESERVA: ");
+    Console.WriteLine(reserva);
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Erro na reserva: " + e.Message);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Formado errado: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Erro inesperado: " + e.Message);
+}
---
+++ ExcecoesPersonalizadas1/Reserva.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcecoesPersonalizadas1.Entities.Exceptions;
+
+namespace ExcecoesPersonalizadas1
+{
+    internal class Reserva
+    {
+        public int NumeroQuarto { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reserva() { }
+        public Reserva(int numeroQuarto, DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new DomainException("As datas de saída devem ser posteriores às datas de entrada.");
+            }
+
+            NumeroQuarto = numeroQuarto;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duracao()
+        {
+            TimeSpan duracao = Checkout.Subtract(Checkin);
+            return (int)duracao.TotalDays;
+        }
+
+        public void AtualizarReserva(DateTime checkin, DateTime checkout)
+        {
+            if (checkin < DateTime.Now || checkout < DateTime.Now)
+            {
+                throw new DomainException("As datas de atualização devem ser datas futuras.");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("As datas de saída devem ser posteriores às datas de entrada.");
+            }
+
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder();
+            s.Append("Quarto ");
+            s.Append(NumeroQuarto.ToString());
+            s.Append(", Checkin: ");
+            s.Append(Checkin.ToString("dd/MM/yyyy"));
+            s.Append(", Checkout: ");
+            s.Append(Checkout.ToString("dd/MM/yyyy"));
+            s.Append(", ");
+            s.Append(Duracao().ToString());
+            s.Append(" noites");
+            return s.ToString();
+        }
+    }
+}
